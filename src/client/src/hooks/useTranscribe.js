b5fb42cff73e1a921e166b66bc2c9e4ff7d4009d
enricop89 <enrico.portolan@gmail.com>
2022-02-08 15:45:07 +0000

    EnablePartialResultsStabilization

--- src/client/src/hooks/useTranscribe.js
+++ src/client/src/hooks/useTranscribe.js
@@ -108,6 +108,7 @@ export default function useTranscribe({
       // high-quality audio. The sample rate must match the sample rate in the audio file.
       MediaSampleRateHertz: 44100,
       AudioStream: encodedAudioStream,
+      EnablePartialResultsStabilization: true,
       PartialResultsStability: 'high',
       VocabularyFilterName: 'ProfanityModerationList'
     });
